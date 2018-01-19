922f05cb1d3bc66d1d6c769e2819d9f1cfaabb3e
Syed Saad <hassan.saad.mail@gmail.com>
2018-01-20 00:30:04 +0530

    fix: Sidebar Link changed to hyperlinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
+import { toggleAll, toggleCollapsed } from '../../redux/App/actions';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -9,60 +11,70 @@ type Props = {
   collapsed: boolean,
   onCollapse: Function,
 };
-function Sidebar(props: Props) {
-  const { collapsed, onCollapse } = props;
-  return (
-    <Sider
-      style={{ minWidth: 50 }}
-      className="sidebar-container"
-      collapsible
-      collapsed={collapsed}
-      onCollapse={() => props.onCollapse()}
-    >
-      <div className="sidebar-top">
-        <Link to="/dashboard">
-          {collapsed ? (
-            <span className="title title--collapsed">Cu</span>
-          ) : (
-            <span className="title">Curely</span>
-          )}
-        </Link>
-      </div>
-      <Menu className="sidebar-menu" theme="dark" defaultSelectedKeys={[]} mode="inline">
-        <Menu.Item key="1">
-          <Link to="/dashboard">
-            <Icon>
-              <icon className="ion-home" />
-            </Icon>
-            <span>Home</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="patients">
-          <Link to="/dashboard/patients">
-            <Icon>
-              <icon className="ion-ios-list-outline" />
-            </Icon>
-            <span>Patients</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link to="/dashboard/profile">
-            <Icon>
-              <icon className="ion-person" />
-            </Icon>
-            <span>My Profile</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Link to="/dashboard/settings">
-            <Icon>
-              <icon className="ion-gear-b" />
-            </Icon>
-            <span>Settings</span>
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-  );
+class Sidebar extends React.Component {
+  state = {};
+  render() {
+    const { collapsed } = this.props.app;
+    return (
+      <Sider
+        className="sidebar-container"
+        collapsed={collapsed}
+        onCollapse={() => this.props.toggleCollapsed()}
+      >
+        <div className="sidebar-top">
+          <a href="/dashboard">
+            {collapsed ? (
+              <span className="title title--collapsed">Cu</span>
+            ) : (
+              <span className="title">Curely</span>
+            )}
+          </a>
+        </div>
+        <Menu className="sidebar-menu" theme="dark" defaultSelectedKeys={[]} mode="inline">
+          <Menu.Item key="1">
+            <a href="/dashboard">
+              <Icon>
+                <icon className="ion-home" />
+              </Icon>
+              <span>Home</span>
+            </a>
+          </Menu.Item>
+          <Menu.Item key="patients">
+            <a href="/dashboard/patients">
+              <Icon>
+                <icon className="ion-ios-list-outline" />
+              </Icon>
+              <span>Patients</span>
+            </a>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <a href="/dashboard/profile">
+              <Icon>
+                <icon className="ion-person" />
+              </Icon>
+              <span>My Profile</span>
+            </a>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <a href="/dashboard/settings">
+              <Icon>
+                <icon className="ion-gear-b" />
+              </Icon>
+              <span>Settings</span>
+            </a>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    );
+  }
 }
-export default Sidebar;
+const mapStateToProps = (state) => {
+  const { App } = state;
+  return {
+    app: App,
+  };
+};
+const mapDispatchToProps = dispatch => ({
+  toggleCollapsed: () => dispatch(toggleCollapsed()),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
