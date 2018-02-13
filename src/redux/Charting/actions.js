52d0468b582c762d8e46a663fdea60759571d401
Syed Saad <hassan.saad.mail@gmail.com>
2018-02-14 01:58:01 +0530

    Charts Services Actions and Reducers Update for all types of records

--- src/redux/Charting/actions.js
+++ src/redux/Charting/actions.js
@@ -11,6 +11,46 @@ export const actions = {
   APPOINTMENT_VITAL_SIGNS_DELETE_SUCCESS: 'APPOINTMENT_VITAL_SIGNS_DELETE_SUCCESS',
   APPOINTMENT_VITAL_SIGNS_DELETE_FAILED: 'APPOINTMENT_VITAL_SIGNS_DELETE_FAILED',
 
+  APPOINTMENT_CLINICAL_NOTES_UPDATE_REQUEST: 'APPOINTMENT_CLINICAL_NOTES_UPDATE_REQUEST',
+  APPOINTMENT_CLINICAL_NOTES_UPDATE_SUCCESS: 'APPOINTMENT_CLINICAL_NOTES_UPDATE_SUCCESS',
+  APPOINTMENT_CLINICAL_NOTES_UPDATE_FAILED: 'APPOINTMENT_CLINICAL_NOTES_UPDATE_FAILED',
+
+  APPOINTMENT_CLINICAL_NOTES_DELETE_REQUEST: 'APPOINTMENT_CLINICAL_NOTES_DELETE_REQUEST',
+  APPOINTMENT_CLINICAL_NOTES_DELETE_SUCCESS: 'APPOINTMENT_CLINICAL_NOTES_DELETE_SUCCESS',
+  APPOINTMENT_CLINICAL_NOTES_DELETE_FAILED: 'APPOINTMENT_CLINICAL_NOTES_DELETE_FAILED',
+
+  APPOINTMENT_PRESCRIPTIONS_UPDATE_REQUEST: 'APPOINTMENT_PRESCRIPTIONS_UPDATE_REQUEST',
+  APPOINTMENT_PRESCRIPTIONS_UPDATE_SUCCESS: 'APPOINTMENT_PRESCRIPTIONS_UPDATE_SUCCESS',
+  APPOINTMENT_PRESCRIPTIONS_UPDATE_FAILED: 'APPOINTMENT_PRESCRIPTIONS_UPDATE_FAILED',
+
+  APPOINTMENT_PRESCRIPTIONS_DELETE_REQUEST: 'APPOINTMENT_PRESCRIPTIONS_DELETE_REQUEST',
+  APPOINTMENT_PRESCRIPTIONS_DELETE_SUCCESS: 'APPOINTMENT_PRESCRIPTIONS_DELETE_SUCCESS',
+  APPOINTMENT_PRESCRIPTIONS_DELETE_FAILED: 'APPOINTMENT_PRESCRIPTIONS_DELETE_FAILED',
+
+  APPOINTMENT_LAB_ORDERS_UPDATE_REQUEST: 'APPOINTMENT_LAB_ORDERS_UPDATE_REQUEST',
+  APPOINTMENT_LAB_ORDERS_UPDATE_SUCCESS: 'APPOINTMENT_LAB_ORDERS_UPDATE_SUCCESS',
+  APPOINTMENT_LAB_ORDERS_UPDATE_FAILED: 'APPOINTMENT_LAB_ORDERS_UPDATE_FAILED',
+
+  APPOINTMENT_LAB_ORDERS_DELETE_REQUEST: 'APPOINTMENT_LAB_ORDERS_DELETE_REQUEST',
+  APPOINTMENT_LAB_ORDERS_DELETE_SUCCESS: 'APPOINTMENT_LAB_ORDERS_DELETE_SUCCESS',
+  APPOINTMENT_LAB_ORDERS_DELETE_FAILED: 'APPOINTMENT_LAB_ORDERS_DELETE_FAILED',
+
+  APPOINTMENT_TREATMENT_PLANS_UPDATE_REQUEST: 'APPOINTMENT_TREATMENT_PLANS_UPDATE_REQUEST',
+  APPOINTMENT_TREATMENT_PLANS_UPDATE_SUCCESS: 'APPOINTMENT_TREATMENT_PLANS_UPDATE_SUCCESS',
+  APPOINTMENT_TREATMENT_PLANS_UPDATE_FAILED: 'APPOINTMENT_TREATMENT_PLANS_UPDATE_FAILED',
+
+  APPOINTMENT_TREATMENT_PLANS_DELETE_REQUEST: 'APPOINTMENT_TREATMENT_PLANS_DELETE_REQUEST',
+  APPOINTMENT_TREATMENT_PLANS_DELETE_SUCCESS: 'APPOINTMENT_TREATMENT_PLANS_DELETE_SUCCESS',
+  APPOINTMENT_TREATMENT_PLANS_DELETE_FAILED: 'APPOINTMENT_TREATMENT_PLANS_DELETE_FAILED',
+
+  APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_REQUEST: 'APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_REQUEST',
+  APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_SUCCESS: 'APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_SUCCESS',
+  APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_FAILED: 'APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_FAILED',
+
+  APPOINTMENT_COMPLETED_PROCEDURES_DELETE_REQUEST: 'APPOINTMENT_COMPLETED_PROCEDURES_DELETE_REQUEST',
+  APPOINTMENT_COMPLETED_PROCEDURES_DELETE_SUCCESS: 'APPOINTMENT_COMPLETED_PROCEDURES_DELETE_SUCCESS',
+  APPOINTMENT_COMPLETED_PROCEDURES_DELETE_FAILED: 'APPOINTMENT_COMPLETED_PROCEDURES_DELETE_FAILED',
+
   TOGGLE_DONE_ACTION: 'TOGGLE_DONE_ACTION',
 };
 
@@ -35,6 +75,121 @@ export const deleteVitalSignsSuccess = data => ({
 });
 export const deleteVitalSignsFailed = error => ({
   type: actions.APPOINTMENT_VITAL_SIGNS_DELETE_FAILED,
+  payload: error,
+});
+export const updateClinicalNotesRequest = () => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_UPDATE_REQUEST,
+});
+export const updateClinicalNotesSuccess = data => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_UPDATE_SUCCESS,
+  payload: data,
+});
+export const updateClinicalNotesFailed = error => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_UPDATE_FAILED,
+  payload: error,
+});
+
+export const deleteClinicalNotesRequest = () => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_DELETE_REQUEST,
+});
+export const deleteClinicalNotesSuccess = data => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_DELETE_SUCCESS,
+  payload: data,
+});
+export const deleteClinicalNotesFailed = error => ({
+  type: actions.APPOINTMENT_CLINICAL_NOTES_DELETE_FAILED,
+  payload: error,
+});
+export const updatePrescriptionsRequest = () => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_UPDATE_REQUEST,
+});
+export const updatePrescriptionsSuccess = data => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_UPDATE_SUCCESS,
+  payload: data,
+});
+export const updatePrescriptionsFailed = error => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_UPDATE_FAILED,
+  payload: error,
+});
+
+export const deletePrescriptionsRequest = () => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_DELETE_REQUEST,
+});
+export const deletePrescriptionsSuccess = data => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_DELETE_SUCCESS,
+  payload: data,
+});
+export const deletePrescriptionsFailed = error => ({
+  type: actions.APPOINTMENT_PRESCRIPTIONS_DELETE_FAILED,
+  payload: error,
+});
+export const updateLabOrdersRequest = () => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_UPDATE_REQUEST,
+});
+export const updateLabOrdersSuccess = data => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_UPDATE_SUCCESS,
+  payload: data,
+});
+export const updateLabOrdersFailed = error => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_UPDATE_FAILED,
+  payload: error,
+});
+
+export const deleteLabOrdersRequest = () => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_DELETE_REQUEST,
+});
+export const deleteLabOrdersSuccess = data => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_DELETE_SUCCESS,
+  payload: data,
+});
+export const deleteLabOrdersFailed = error => ({
+  type: actions.APPOINTMENT_LAB_ORDERS_DELETE_FAILED,
+  payload: error,
+});
+export const updateTreatmentPlansRequest = () => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_UPDATE_REQUEST,
+});
+export const updateTreatmentPlansSuccess = data => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_UPDATE_SUCCESS,
+  payload: data,
+});
+export const updateTreatmentPlansFailed = error => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_UPDATE_FAILED,
+  payload: error,
+});
+
+export const deleteTreatmentPlansRequest = () => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_DELETE_REQUEST,
+});
+export const deleteTreatmentPlansSuccess = data => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_DELETE_SUCCESS,
+  payload: data,
+});
+export const deleteTreatmentPlansFailed = error => ({
+  type: actions.APPOINTMENT_TREATMENT_PLANS_DELETE_FAILED,
+  payload: error,
+});
+export const updateCompletedProceduresRequest = () => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_REQUEST,
+});
+export const updateCompletedProceduresSuccess = data => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_SUCCESS,
+  payload: data,
+});
+export const updateCompletedProceduresFailed = error => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_UPDATE_FAILED,
+  payload: error,
+});
+
+export const deleteCompletedProceduresRequest = () => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_DELETE_REQUEST,
+});
+export const deleteCompletedProceduresSuccess = data => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_DELETE_SUCCESS,
+  payload: data,
+});
+export const deleteCompletedProceduresFailed = error => ({
+  type: actions.APPOINTMENT_COMPLETED_PROCEDURES_DELETE_FAILED,
   payload: error,
 });
 export const toggleDoneAction = () => ({
@@ -64,5 +219,135 @@ export const deleteVitalSigns = appointmentId => (dispatch) => {
     })
     .catch((error) => {
       dispatch(deleteVitalSignsFailed(error));
+    });
+};
+export const updateClinicalNotes = data => (dispatch) => {
+  dispatch(updateClinicalNotesRequest());
+  service
+    .updateClinicalNotes(data)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(updateClinicalNotesSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(updateClinicalNotesFailed(error));
+    });
+};
+export const deleteClinicalNotes = appointmentId => (dispatch) => {
+  dispatch(deleteClinicalNotesRequest());
+  service
+    .deleteClinicalNotes(appointmentId)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(deleteClinicalNotesSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(deleteClinicalNotesFailed(error));
+    });
+};
+export const updatePrescriptions = data => (dispatch) => {
+  dispatch(updatePrescriptionsRequest());
+  service
+    .updatePrescriptions(data)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(updatePrescriptionsSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(updatePrescriptionsFailed(error));
+    });
+};
+export const deletePrescriptions = appointmentId => (dispatch) => {
+  dispatch(deletePrescriptionsRequest());
+  service
+    .deletePrescriptions(appointmentId)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(deletePrescriptionsSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(deletePrescriptionsFailed(error));
+    });
+};
+export const updateLabOrders = data => (dispatch) => {
+  dispatch(updateLabOrdersRequest());
+  service
+    .updateLabOrders(data)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(updateLabOrdersSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(updateLabOrdersFailed(error));
+    });
+};
+export const deleteLabOrders = appointmentId => (dispatch) => {
+  dispatch(deleteLabOrdersRequest());
+  service
+    .deleteLabOrders(appointmentId)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(deleteLabOrdersSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(deleteLabOrdersFailed(error));
+    });
+};
+export const updateTreatmentPlans = data => (dispatch) => {
+  dispatch(updateTreatmentPlansRequest());
+  service
+    .updateTreatmentPlans(data)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(updateTreatmentPlansSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(updateTreatmentPlansFailed(error));
+    });
+};
+export const deleteTreatmentPlans = appointmentId => (dispatch) => {
+  dispatch(deleteTreatmentPlansRequest());
+  service
+    .deleteTreatmentPlans(appointmentId)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(deleteTreatmentPlansSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(deleteTreatmentPlansFailed(error));
+    });
+};
+export const updateCompletedProcedures = data => (dispatch) => {
+  dispatch(updateCompletedProceduresRequest());
+  service
+    .updateCompletedProcedures(data)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(updateCompletedProceduresSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(updateCompletedProceduresFailed(error));
+    });
+};
+export const deleteCompletedProcedures = appointmentId => (dispatch) => {
+  dispatch(deleteCompletedProceduresRequest());
+  service
+    .deleteCompletedProcedures(appointmentId)
+    .then(checkStatus)
+    .then(checkResponseCode)
+    .then((response) => {
+      dispatch(deleteCompletedProceduresSuccess(response.Payload));
+    })
+    .catch((error) => {
+      dispatch(deleteCompletedProceduresFailed(error));
     });
 };
--- src/services/charts.js
+++ src/services/charts.js
@@ -14,6 +14,70 @@ class ChartsService {
     );
     return fetch(request);
   }
+  static updateClinicalNotes(data) {
+    const request = postRequest(_config.baseUrl, '/admin/appointment/clinicalnotes/update', data);
+    return fetch(request);
+  }
+  static deleteClinicalNotes(appointmentId) {
+    const request = getRequest(
+      _config.baseUrl,
+      `/admin/appointment/clinicalnotes/delete/${appointmentId}`,
+      true,
+    );
+    return fetch(request);
+  }
+  static updatePrescriptions(data) {
+    const request = postRequest(_config.baseUrl, '/admin/appointment/prescriptions/update', data);
+    return fetch(request);
+  }
+  static deletePrescriptions(appointmentId) {
+    const request = getRequest(
+      _config.baseUrl,
+      `/admin/appointment/prescriptions/delete/${appointmentId}`,
+      true,
+    );
+    return fetch(request);
+  }
+  static updateLabOrders(data) {
+    const request = postRequest(_config.baseUrl, '/admin/appointment/laborders/update', data);
+    return fetch(request);
+  }
+  static deleteLabOrders(appointmentId) {
+    const request = getRequest(
+      _config.baseUrl,
+      `/admin/appointment/laborders/delete/${appointmentId}`,
+      true,
+    );
+    return fetch(request);
+  }
+  static updateTreatmentPlans(data) {
+    const request = postRequest(_config.baseUrl, '/admin/appointment/treatmentplans/update', data);
+    return fetch(request);
+  }
+  static deleteTreatmentPlans(appointmentId) {
+    const request = getRequest(
+      _config.baseUrl,
+      `/admin/appointment/treatmentplans/delete/${appointmentId}`,
+      true,
+    );
+    return fetch(request);
+  }
+  static updateCompletedProcedures(data) {
+    const request = postRequest(
+      _config.baseUrl,
+      '/admin/appointment/completedprocedures/update',
+      data,
+    );
+    return fetch(request);
+  }
+  static deleteCompletedProcedures(appointmentId) {
+    const request = getRequest(
+      _config.baseUrl,
+      `/admin/appointment/completedprocedures/delete/${appointmentId}`,
+      true,
+    );
+    return fetch(request);
+  }
 }
 
 export default ChartsService;
